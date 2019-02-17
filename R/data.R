ea59c9aa218b5f10ef9fbaa323e6157229814cf4
Neal Grantham <neal@nsgrantham.com>
2019-02-16 18:33:18 -0800

    Update README, add CI

--- R/data.R
+++ R/data.R
@@ -1,30 +1,27 @@
-#' @importFrom tibble tibble
-NULL
-
 #' US population data, 1900-2018
 #'
 #' @format A data frame (specifically a tbl_df) with 118 observations and 2 variables: 
-#'         \code{year} and \code{pop}, the US population on July 1st of the given year 
-#'         as estimated by the US Census Bureau. Two exceptions are 1970 and 1980 where 
-#'         \code{pop} is the estimated US population on April 1st. For years before 1970,
-#'         \code{pop} is rounded to the nearest thousandth. From 1900 to 1949, the US 
-#'         population estimate includes the first 48 states and District of Columbia.
-#'         From 1950 to 2018, the US population includes the first 48 states, Alaska
-#'         (granted statehood in 1959), Hawaii (granted statehood in 1959), and 
-#'         District of Columbia. The US territory Puerto Rico is not included any 
-#'         US population estimate.
+#'   \code{year} and \code{pop}, the US population on July 1st of the given year 
+#'   as estimated by the US Census Bureau. Two exceptions are 1970 and 1980 where 
+#'   \code{pop} is the estimated US population on April 1st. For years before 1970,
+#'   \code{pop} is rounded to the nearest thousandth. From 1900 to 1949, the US 
+#'   population estimate includes the first 48 states and District of Columbia.
+#'   From 1950 to 2018, the US population includes the first 48 states, Alaska
+#'   (granted statehood in 1959), Hawaii (granted statehood in 1959), and 
+#'   District of Columbia. The US territory Puerto Rico is not included any 
+#'   US population estimate.
 "us_pops"
 
 #' State population data, 1900-2018
 #'
 #' @format A data frame (specifically a tbl_df) with 5,927 observations and 3 variables: 
-#'         \code{year}, \code{state}, and \code{pop}, the population of the given state 
-#'         on July 1st of the given year as estimated by the US Census Bureau. Two 
-#'         exceptions are 1970 and 1980 where \code{pop} is the estimated state population 
-#'         on April 1st. For years before 1970, \code{pop} is rounded to the nearest thousandth.
-#'         Population estimates for the first 48 states and District of Columbia are included 
-#'         for all years. Population estimates for Alaska (granted statehood in 1959) are 
-#'         included beginning in 1950. Population estimates for Hawaii (granted statehood in 
-#'         1959) are included beginning in 1950. Population estimates for the US territory 
-#'         Puerto Rico are included beginning in 2010.
+#'   \code{year}, \code{state}, and \code{pop}, the population of the given state 
+#'   on July 1st of the given year as estimated by the US Census Bureau. Two 
+#'   exceptions are 1970 and 1980 where \code{pop} is the estimated state population 
+#'   on April 1st. For years before 1970, \code{pop} is rounded to the nearest thousandth.
+#'   Population estimates for the first 48 states and District of Columbia are included 
+#'   for all years. Population estimates for Alaska (granted statehood in 1959) are 
+#'   included beginning in 1950. Population estimates for Hawaii (granted statehood in 
+#'   1959) are included beginning in 1950. Population estimates for the US territory 
+#'   Puerto Rico are included beginning in 2010.
 "state_pops"
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# us-population-data
-🇺🇸 Scripts to get U.S. Census Bureau data on population estimates by state (1970 to 2019) and county (1990 to 2019).
+# uspops 🇺🇸
+
+[![Travis-CI Build Status](https://travis-ci.org/nsgrantham/uspops.svg?branch=master)](https://travis-ci.org/nsgrantham/uspops)
+[![AppVeyor Build Status](https://ci.appveyor.com/api/projects/status/github/nsgrantham/uspops?branch=master&svg=true)](https://ci.appveyor.com/project/nsgrantham/uspops)
+
+This package contains two datasets provided by the US Census Bureau:
+
+* `us_pops`: For each year from 1900 to 2018, the population estimate of 
+  the United States on July 1st (except for 1970 and 1980 where the 
+  estimate is given for April 1st).
+
+* `state_pops`: For each year from 1900 to 2018, the population estimate 
+  of District of Columbia, the first 48 states, Alaska (granted statehood in
+  1959, estimates since 1950), Hawaii (granted statehood in 1959, estimates 
+  since 1950), and the US territory Puerto Rico (estimates since 2010).
+
+The datasets are tidy and made from the following non-tidy US Census Bureau files:
+
+* [Population estimates 1900-1909](https://www2.census.gov/programs-surveys/popest/tables/1980-1990/state/asrh/st0009ts.txt)
+
+* [Population estimates 1910-1919](https://www2.census.gov/programs-surveys/popest/tables/1980-1990/state/asrh/st1019ts.txt)
+
+* [Population estimates 1920-1929](https://www2.census.gov/programs-surveys/popest/tables/1980-1990/state/asrh/st2029ts.txt)
+
+* [Population estimates 1930-1939](https://www2.census.gov/programs-surveys/popest/tables/1980-1990/state/asrh/st3039ts.txt)
+
+* [Population estimates 1940-1949](https://www2.census.gov/programs-surveys/popest/tables/1980-1990/state/asrh/st4049ts.txt)
+
+* [Population estimates 1950-1959](https://www2.census.gov/programs-surveys/popest/tables/1980-1990/state/asrh/st5060ts.txt)
+
+* [Population estimates 1960-1969](https://www2.census.gov/programs-surveys/popest/tables/1980-1990/state/asrh/st6070ts.txt)
+
+* [Population estimates 1970-1979](https://www2.census.gov/programs-surveys/popest/tables/1980-1990/state/asrh/st7080ts.txt)
+
+* [Population estimates 1980-1989](https://www2.census.gov/programs-surveys/popest/tables/1980-1990/state/asrh/st8090ts.txt)
+
+* [Population estimates 1990-1999](https://www2.census.gov/programs-surveys/popest/tables/1990-2000/intercensal/st-co/co-est2001-12-00.pdf)
+
+* [Population estimates 2000-2009](https://www2.census.gov/programs-surveys/popest/datasets/2000-2009/national/totals/nst_est2009_alldata.csv)
+
+* [Population estimates 2010-2018](https://www2.census.gov/programs-surveys/popest/datasets/2010-2018/national/totals/nst-est2018-alldata.csv)
+
+To produce `us_pops` and `state_pops` from these files, see [`data-raw/get-data.R`](data-raw/get-data.R).
+
+## Installation
+
+You can install uspops from GitHub with:
+
+```r
+# install.packages("devtools")
+devtools::install_github("nsgrantham/uspops")
+```
